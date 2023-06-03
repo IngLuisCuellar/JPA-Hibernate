4381583c9a49aebd02ece1938be336c7e96bc37a
Luis Cuellar <luisenri2901@gmail.com>
2023-06-03 15:37:09 -0500

    Se agregan las anotaciones que identifican los atributos en la tabla, como el Id y el valor y tipo que tendrá este, además se conoce la anotación necesaria para igualar los nombres que tienen los atributos en la clase y en la tabla.

--- src/main/java/com/latam/alura/tienda/modelo/Producto.java
+++ src/main/java/com/latam/alura/tienda/modelo/Producto.java
@@ -1,16 +1,50 @@
 package com.latam.alura.tienda.modelo;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity  //Anotación que nos indica que esta es una entidad que realiza mapeamientos
-@Table(name = "productos") //Esta anotaciòn permite poner el nombre de representación en la tabla
+@Table(name = "productos") //Esta anotaciòn permite poner el nombre de representación de la tabla Nombre tabla, nombre clase
 
 public class Producto {
+
+    @Id //Identificador del Id de la base de datos
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Se genera en la base de datos según el tipo
     private long id;
+    //@Column(name = "Si tuvieran un nombre diferente")
     private String nombre;
     private String descripcion;
     private BigDecimal precio;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
 }
