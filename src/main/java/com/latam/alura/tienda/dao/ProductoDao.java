4c5e6d0a7b56d55e66c3859611f9d45f61e0b542
Luis Cuellar <luisenri2901@gmail.com>
2023-06-08 05:55:25 -0500

    Se agrega un metodo de consulta por parámetros que recibe el nombre, precio y fecha. En el desarrollo de este se introducen los StringBuilder, que permiten modificar el String, además del .append() a estos, permitiendo devolver los parámetros que no sean nulls. Por otro lado, la clase PruebaDeParametros fue copiada del recurso

--- src/main/java/com/latam/alura/tienda/dao/ProductoDao.java
+++ src/main/java/com/latam/alura/tienda/dao/ProductoDao.java
@@ -3,7 +3,9 @@ package com.latam.alura.tienda.dao;
 import com.latam.alura.tienda.modelo.Producto;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 public class ProductoDao {
@@ -40,5 +42,32 @@ public class ProductoDao {
     public BigDecimal consultaPrecioPorNombreDeProducto(String nombre){
         String jpql = "SELECT P.precio FROM Producto AS P WHERE P.nombre=:nombre";
         return em.createNamedQuery("Producto.consultaDePrecio", BigDecimal.class).setParameter("nombre", nombre).getSingleResult();
+    }
+
+    public List<Producto> consultaPorParametro(String nombre, BigDecimal precio, LocalDate fecha){
+        StringBuilder jpql = new StringBuilder("SELECT  p FROM Producto p WHERE 1=1 "); //Permite modificar el string
+        if (nombre != null && !nombre.trim().isEmpty()) { //!nombre.trim().isEmpty() nombre que no sea vacio
+            jpql.append("AND p.nombre=:nombre "); //Se va modificando la consulta a medida que se cumplan las condiciones
+        }
+        if (precio != null && !precio.equals(new BigDecimal(0))){
+            jpql.append("AND p.precio=:precio ");
+        }
+        if (fecha != null){
+            jpql.append("AND p.fechaDeRegistro=:fecha");
+        }
+
+        TypedQuery<Producto> query = em.createQuery(jpql.toString(), Producto.class);
+
+        if (nombre != null && !nombre.trim().isEmpty()) { //!nombre.trim().isEmpty() nombre que no sea vacio
+            query.setParameter("nombre", nombre);
+        }
+        if (precio != null && !precio.equals(new BigDecimal(0))){
+            query.setParameter("precio", precio);
+        }
+        if (fecha != null){
+            query.setParameter("fechaDeRegistro", fecha);
+        }
+
+        return query.getResultList();
     }
 }
